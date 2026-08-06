0c223750c28ac088d1642ec5bc2d38190c4fa085
murilohiulgabr <murilohiulgabr@gmail.com>
2026-08-06 09:10:10 -0300

    Organização de fotos Durtoc

--- durtoc/durtoc.js
+++ durtoc/durtoc.js
@@ -5,8 +5,8 @@ const dadosVertentes = {
         setor: "Níveis Inferiores / Zona Industrial de Coruscant",
         autorizacao: "Acesso Civil Restrito (Sob Vigilância do ISB)",
         lore: "Mecânico ríspido, ranzinza e incrivelmente genial que operava uma humilde oficina cravada no limiar cinzento dos subníveis de Coruscant. Reconhecido no submundo por seu absoluto desdém por burocratas e por sua capacidade milagrosa de ouvir e diagnosticar falhas moleculares em motores e compressores iônicos. Fundador da lendária 'Brigada das Catracas', utilizava sucatas recondicionadas e ferramentas modificadas para construir engenharia de sobrevivência pura, dando vida à nave-base 'A Engrenagem Mestra' e ao caça leve personalizado 'O Cinzel'. Assombrado pelo confisco do Kalicore de sua linhagem, canalizava sua fúria na criação de droids e modificações de alta performance que superavam as especificações originais dos fabricantes.",
-        fotoCorpo: "../img/DurtocMec.png",
-        fotoDoc: "../img/DurtocDocumento.png",
+        fotoCorpo: "../img/personagens/durtocFotos/DurtocMec.png",
+        fotoDoc: "../img/personagens/durtocFotos/DurtocDocumento.png",
         mostrarFabricas: false
     },
     general: {
@@ -15,8 +15,8 @@ const dadosVertentes = {
         setor: "Setor Industrial Avançado, Cidade Capital (Planeta Lothal)",
         autorizacao: "Nível Alfa / Código Vermelho de Comando Galáctico",
         lore: "Decorridos nove anos de operações implacáveis e expansão tecnológica massiva, Dur'toc acumulou capital e prestígio galáctico incontestáveis, expandindo sua influência com a compra e reabertura de uma segunda e seleta oficina mecânica na alta sociedade de Coruscant. Sua fama como o engenheiro mais meticuloso da galáxia chamou a atenção direta do Alto Comando Imperial e do Grande Almirante Thrawn. Convocado para o planeta industrial Lothal, Dur'toc assumiu a prestigiada patente de General e o cargo crítico de Engenheiro Mecânico Chefe das Indústrias Imperiais. Atualmente, ele é a autoridade técnica final sobre três colossais fábricas de produção em massa na Cidade Capital de Lothal, sendo o responsável direto pela montagem tática, calibração e testes de voo secretos do revolucionário caça estelar experimental TIE Defender.",
-        fotoCorpo: "../img/DurtocGeneral.png",
-        fotoDoc: "../img/DurtocGenDocumento.png",
+        fotoCorpo: "../img/personagens/durtocFotos/DurtocGeneral.png",
+        fotoDoc: "../img/personagens/durtocFotos/DurtocGenDocumento.png",
         mostrarFabricas: true
     }
 };
