2957948fa8c4185f882df81194278b7e878ff8e6
murilohiulgabr <murilohiulgabr@gmail.com>
2026-09-14 02:14:25 -0300

    atualização do bash de sincronização entre as branches

--- backups/sincronizacaoBranches.bash
+++ backups/sincronizacaoBranches.bash
@@ -1,3 +1,5 @@
+# Staging to Main Synchronization Script
+
 # 1. Garanta que todas as alterações na staging foram commitadas e enviadas
 git checkout staging
 git push origin staging
@@ -13,3 +15,23 @@ git merge staging
 
 # 5. Envie a main atualizada para o repositório remoto
 git push origin main
+
+# ==========================================================================================
+
+# Main to Staging Synchronization Script
+
+# 1. Garanta que todas as alterações na staging foram commitadas e enviadas
+git checkout main
+git push origin main
+
+# 2. Mude para a branch main
+git checkout staging
+
+# 3. Puxe as últimas atualizações da main remota (boa prática para evitar conflitos)
+git pull origin staging
+
+# 4. Mescle o conteúdo da staging dentro da main
+git merge main
+
+# 5. Envie a staging atualizada para o repositório remoto
+git push origin staging
